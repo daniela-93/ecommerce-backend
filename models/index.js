126ebd235bf67966b5cb9005a0ba878548cf3601
daniela <daniela.acuna93@gmail.com>
2021-05-07 14:52:13 -0500

    finishing models

--- models/index.js
+++ models/index.js
@@ -1,16 +1,44 @@
 // import models
+
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreginKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreginKey: 'product_id'
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreginKey: 'tag_id'
+});
+
+ProductTag.belongsTo(Product, {
+  foreignKey: 'product_id'
+});
+
+ProductTag.belongsTo(Tag, {
+  foreignKey: 'tag_id'
+});
+
+Tag.hasMany(ProductTag, {
+  foreignKey: 'tag_id'
+});
+
+Product.hasMany(ProductTag, {
+  foreginKey: 'product_id'
+});
 
 module.exports = {
   Product,
--- server.js
+++ server.js
@@ -11,17 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
-// import the Sequelize constructor from the library
-const Sequelize = require('sequelize');
-require('dotenv').config();
-// create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequelize('store_db', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
-});
-
-module.exports = sequelize;
